619dc8ca0222d4f012a59c7ceb42e07493eb22c9
nocherrypicking-ra <no.cherrypicking@gmail.com>
2026-05-02 17:24:56 -0500

    Update page.tsx

--- app/tickets/player/page.tsx
+++ app/tickets/player/page.tsx
@@ -28,3 +28,13 @@ export default function Player() {
     </div>
   );
 }
+const container = {
+  minHeight: "100vh",
+  background: "black",
+  color: "white",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  gap: "10px"
+};
