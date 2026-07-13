4d3da9a77299311e0258ae7a75ddb3d57fe02453
nocherrypicking-ra <no.cherrypicking@gmail.com>
2026-07-13 11:29:50 -0500

    Delete app/watchlist/page.tsx

--- app/watchlist/page.tsx
+++
@@ -1,6 +0,0 @@
-import WatchlistClient from "./WatchlistClient";
-import { players } from "./_data/watchlist";
-
-export default function WatchlistPage() {
-  return <WatchlistClient data={players} />;
-}
